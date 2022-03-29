0fb5042c2a160d2ddd03fa5188127762f89ebc71
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2022-03-29 12:52:22 +0530

    Some additions

--- page/app/editor/index.js
+++ page/app/editor/index.js
@@ -1,34 +1,70 @@
-let title = "Enter title here";
+let currentIndex = 0
+let saved = false
 
-let options = [
-    "Enter option here",
-    "Enter option here",
-    "Enter option here",
-    "Enter option here",
-]
+let titles = [
+    "Enter title here"
+];
 
-let correct_options = [
-    false,
-    false,
-    false,
-    false,
-]
+let options = {
+    0: [
+        "Enter option here",
+        "Enter option here",
+        "Enter option here",
+        "Enter option here"
+    ],
+}
+
+let correct_options = {
+    0: [
+        false,
+        false,
+        false,
+        false
+    ],
+}
 
 function setTitle(value){
-    title = value;
+    titles[currentIndex] = value;
 }
 
 function setOption(index, value){
-    options[index] = value;
+    options[currentIndex][index] = value;
 }
 
 function setCorrect(index, value){
-    correct_options[index] = value;
+    correct_options[currentIndex][index] = value;
+}
+
+function incrementIndex(){
+    currentIndex++;
+    if(currentIndex >= titles.length){
+        titles.push("Enter title here");
+        options[currentIndex] = [
+            "Enter option here",
+            "Enter option here",
+            "Enter option here",
+            "Enter option here"
+        ];
+        correct_options[currentIndex] = [
+            false,
+            false,
+            false,
+            false
+        ];
+    }
+    clearFields()
+}
+
+function decrementIndex(){
+    if (currentIndex > 0){
+        currentIndex--;
+        clearFields()
+    }
 }
 
 function save_question(){
     let question = {
-        title: title,
+        titles: titles,
         options: options,
         correct_options: correct_options,
         creator: userID,
@@ -36,4 +72,24 @@ function save_question(){
     }
     //console.log(question);
     quizRef.set(question);
+    firestore.collection("quizzes").doc("_nQuizzes").update({
+        quizzes: nQuizzes + 1
+    });
+    nQuizzes ++
+    saved = true;
+
+    clearFields();
+}
+
+function clearFields(){
+        // clear all fields
+        document.getElementById("title_input").value = "Enter title here";
+        document.getElementById("option1").checked = false;
+        document.getElementById("option2").checked = false;
+        document.getElementById("option3").checked = false;
+        document.getElementById("option4").checked = false;
+        document.getElementById("option_text1").value = "Enter option here";
+        document.getElementById("option_text2").value = "Enter option here";
+        document.getElementById("option_text3").value = "Enter option here";
+        document.getElementById("option_text4").value = "Enter option here";
 }
