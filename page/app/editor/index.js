464ec775bb9f121716ecac7ef540725ff72c21c1
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2022-03-30 18:03:04 +0530

    A ton of bugfixes

--- page/app/editor/index.js
+++ page/app/editor/index.js
@@ -51,53 +51,57 @@ function incrementIndex(){
             false,
             false
         ];
+        clearFields();
+    } else {
+        updateFields(currentIndex);
     }
-    clearFields()
 }
 
 function decrementIndex(){
     if (currentIndex > 0){
         currentIndex--;
-        clearFields()
+        updateFields(currentIndex);
     }
 }
 
-function save_question(){
+async function save_question(){
     let question = {
         titles: titles,
         options: options,
         correct_options: correct_options,
         creator: userID,
-        assignees: [],
         class: current_class,
     }
-    //console.log(question);
-    quizRef.set(question);
-    firestore.collection("quizzes").doc("_nQuizzes").update({
-        quizzes: nQuizzes + 1
-    });
     nQuizzes ++
     saved = true;
+    await quizRef.set(question).then(function(){
+        firestore.collection("quizzes").doc("_nQuizzes").update({
+            quizzes: nQuizzes + 1
+        }).then(function(){
 
-    firestore.collection("classes").doc(current_class).get().then(function(doc){
-        let class_data = doc.data();
-        let quizzes = class_data.quizzes;
-        quizzes.push(quizRef.id);
-        firestore.collection("classes").doc(current_class).update({
-            quizzes: quizzes
+            firestore.collection("classes").doc(current_class).get().then(function(doc){
+                let class_data = doc.data();
+                let quizzes = class_data.quizzes;
+                quizzes.push(quizRef.id);
+                firestore.collection("classes").doc(current_class).update({
+                    quizzes: quizzes
+                });
+            }).then(function(){
+
+                firestore.collection('users').doc(userID).get().then(function(doc){
+                    let user_data = doc.data();
+                    let quizHist = user_data.quiz_history;
+                    quizHist.push(quizHist[quizHist.length - 1] + 1);
+                    firestore.collection('users').doc(userID).update({
+                        quiz_history: quizHist
+                    });
+                }).then(function(){
+                    clearFields();
+                    window.location.href = "../index.html"
+                });
+            });
         });
     });
-
-    firestore.collection('users').doc(userID).get().then(function(doc){
-        let user_data = doc.data();
-        let quizHist = user_data.quiz_history;
-        quizHist.push(quizHist[quizHist.length - 1] + 1);
-        firestore.collection('users').doc(userID).update({
-            quiz_history: quizHist
-        });
-    });
-    clearFields();
-    window.location.href = "../index.html"
 }
 
 function clearFields(){
@@ -111,4 +115,16 @@ function clearFields(){
         document.getElementById("option_text2").value = "Enter option here";
         document.getElementById("option_text3").value = "Enter option here";
         document.getElementById("option_text4").value = "Enter option here";
+}
+
+function updateFields(index){
+    document.getElementById("title_input").value = titles[index];
+    document.getElementById("option1").checked = correct_options[index][0];
+    document.getElementById("option2").checked = correct_options[index][1];
+    document.getElementById("option3").checked = correct_options[index][2];
+    document.getElementById("option4").checked = correct_options[index][3];
+    document.getElementById("option_text1").value = options[index][0];
+    document.getElementById("option_text2").value = options[index][1];
+    document.getElementById("option_text3").value = options[index][2];
+    document.getElementById("option_text4").value = options[index][3];
 }
